ff671f96ca7d1c136390bf05f26a14a2cfcc807f
JeffHilde <j.hilde@outlook.com>
2022-01-16 19:51:44 -0500

    create lib

---
+++ CORE/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CORE
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
---
+++ CORE/Platforms/Android/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace CORE
+{
+    // All the code in this file is only included on Android.
+    public class PlatformClass1
+    {
+    }
+}
