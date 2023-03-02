c95c42570e29d37c050d7c8be981d3df862ad638
Jeff Hilde <Jeff@swopblock.org>
2023-03-02 16:55:21 -0500

    adding tests for order

--- order/order.cs
+++ order/order.cs
@@ -1,2 +1,151 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Linq.Expressions;
+
+Console.WriteLine("Hello, Swopblock Core Ordering World!");
+
+var orderModel = new OrderModel(Console.Out, Console.In);
+
+orderModel.ProcessOrders();
+
+Invoice Invoice= new Invoice();
+
+public class OrderModel
+{
+    TextWriter Writer;
+
+    TextReader Reader;
+
+    public OrderModel()
+    {
+
+    }
+
+    public OrderModel(TextWriter writer, TextReader reader)
+    {
+        Writer = writer;
+
+        Reader = reader;
+    }
+
+    public void ProcessOrders()
+    {
+        string message;
+
+        while ((message = Reader.ReadLine()) != null)
+        {
+            message = ProcessOrder(message);
+
+            Writer.WriteLine(message);
+        }
+    }
+
+    public string ProcessOrder(string line) 
+    {
+        return null;
+    }
+
+    public void OrderSynthesizing()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Writer.WriteLine("I am");
+        }
+    }
+}
+
+public class Offer
+{
+    public Amount Amount { get; init; }
+
+    public Unit OfferUnit { get; init; }    
+
+    public Unit UsingUnit { get; init; }
+
+    public Address Address { get; init; }
+
+    public string Text { get; init; }
+
+    public Offer(Amount amount, Unit offerUnit, Unit usingUnit, Address address)
+    {
+        Amount = amount;
+        OfferUnit = offerUnit;
+        UsingUnit = usingUnit;
+        Address = address;
+
+        Text = Amount.Text + OfferUnit.Text + " using the" + UsingUnit.Text + Address.Text;
+    }
+}
+
+public class Amount
+{
+    public decimal AtMost { get; init; }
+
+    public string Text { get; init; }
+
+    public Amount(decimal atMost)
+    {
+        AtMost = atMost;
+
+        Text = " " + AtMost.ToString();
+    }
+}
+
+public class Address
+{
+    public string Text { get; init; }
+
+    public Address(string text)
+    {
+        Text = " " + " address " + text;
+    }
+}
+
+public class Order
+{
+    public Unit Unit { get; init; }
+    public string Text { get; init; }
+
+    public Order(Unit unit) 
+    {
+        Unit = unit;
+
+        Text = Unit.Text + " using the market.";
+    }
+}
+
+public abstract class Unit
+{
+    public string Text { get; init; }
+}
+
+public class BTC : Unit
+{
+    public BTC()
+    {
+        Text = " BTC";
+    }
+}
+
+public class ETH : Unit
+{
+    public ETH()
+    {
+        Text = " ETH";
+    }
+}
+
+public class USD : Unit
+{
+    public USD()
+    {
+        Text = " USD";
+    }
+}
+
+public class SWOBL : Unit
+{
+    public SWOBL()
+    {
+        Text = " SWOBL";
+    }
+}
--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -3,9 +3,80 @@ namespace test
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestBTCUnit()
         {
+            var unit = new BTC();
 
+            Assert.Equal(unit.Text, "BTC");
+        }
+
+        [Fact]
+        public void TestETHUnit()
+        {
+            var unit = new ETH();
+
+            Assert.Equal(unit.Text, "ETH");
+        }
+
+        [Fact]
+        public void TestUSDUnit()
+        {
+            var unit = new USD();
+
+            Assert.Equal(unit.Text, "USD");
+        }
+
+        [Fact]
+        public void TestSWOBLUnit() 
+        { 
+            var unit = new SWOBL();
+
+            Assert.Equal(unit.Text, "SWOBL");
+        }
+
+        [Fact]
+        public void TestSWOBLOrder()
+        {
+            var unit = new SWOBL();
+
+            var order = new Order(unit);
+
+            Assert.Equal("SWOBL using the market.", order.Text);
+        }
+
+        [Fact]
+        public void TestAmount()
+        {
+            var amount = new Amount(1000);
+
+            Assert.Equal("1000", amount.Text);
+        }
+
+        [Fact]
+        public void TestAddress() 
+        {
+            var address = new Address("0xFB");
+
+            Assert.Equal("0xFB", address.Text);
+
+        }
+
+        [Fact]
+        public void TestOffer()
+        {
+            var amount = new Amount(123);
+
+            var offerUnit = new SWOBL();
+
+            var usingUnit = new BTC();
+
+            var usingAddress = new Address("0x2947");
+
+            var offer = new Offer(amount, offerUnit, usingUnit, usingAddress);
+
+            //Assert
+
+            Assert.Equal("123SWOBLusingtheBTCaddress0x2947", offer.Text);
         }
     }
 }
