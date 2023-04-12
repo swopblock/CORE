facbb404205ce12ff432b7474bdfa57ab2fd159f
Jeff Hilde <Jeff@swopblock.org>
2023-04-12 14:32:04 -0400

    working on order.cs

--- order/order.cs
+++ order/order.cs
@@ -1,6 +1,7 @@
 ﻿//Copyright (c) 2023 Swopblock
 
 using System.Linq.Expressions;
+using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
 
 //Console.WriteLine("Hello, Swopblock Core Ordering World!");
@@ -13,21 +14,185 @@ using System.Reflection.Metadata.Ecma335;
 
 namespace Swopblock
 {
-    public interface IOffer { new IOfferable Make(); }
+    public partial interface IOfferable { }// : IReceipt { IOffer Make(); }
 
-    public partial interface IOfferable { IOfferNetworkable Make(); }
+    public interface IOffer { IOfferable Make(); }
 
-    public partial interface IOfferNetworkable { IInvoiceable Make(); }
+    public partial interface IOfferConfirmable { IInvoiceable Make(); }
 
-    public partial interface IInvoiceable { IInvoice Make(); }
+ 
+    //public partial interface IInvoice { IDeliverable Make(); }
 
-    public partial interface IInvoice { IDeliverable Make(); }
+    //public partial interface IInvoiceDeliverable { IDeliveryNetworkable Make(); }
 
-    public partial interface IDeliverable { IDeliveryNetworkable Make(); }
+    public partial interface IDeliveryConfirmable { IReceiptable Make(); }
 
-    public partial interface IDeliveryNetworkable { IReceiptable Make(); }
+    public partial interface IDelivery { }
 
-    public partial interface IReceiptable { IOffer Make(); }
+
+    public partial interface IReceiptable { }// { IOffer Make(); }
+
+    //public partial interface IReceipt { }
+
+
+    public interface ISignature { }
+
+
+
+
+
+
+    public interface ISignable { void Sign(); }
+
+    public interface IBroadcastable { void Broadcast(); }
+
+    public interface IInvoiceable { }
+    public interface IDeliverable { }
+    public interface IConfirmable { }
+
+
+    public record AnOffer() : ISignable, IBroadcastable, IInvoiceable
+    {
+        public void Sign() { throw new NotImplementedException(); }
+
+        public void Broadcast() { throw new NotImplementedException(); }
+
+        public void Confirm() { throw new NotImplementedException(); }
+
+        public AnInvoice Invoice() { throw new NotImplementedException(); }
+
+    }
+
+    public record AnInvoice()// : ISignable, IDeliverable
+    {
+
+    }
+
+    public record ADelivery() : IReceiptable
+    {
+
+    }
+
+    public record AReceipt() : IOfferable
+    {
+
+    }
+
+    public interface IAppSignOff
+    {
+        OfferModels SignOff(ReceiptModels order);
+
+    }
+
+    public interface IAutoSignOff
+    {
+        DeliveryModels SignOff(InvoiceModels order);
+    }
+
+    public interface IAppBroadcast
+    {
+        OfferModels Broadcast(OfferModels order);
+    }
+
+    public interface IAutoBroadcast
+    {
+        DeliveryModels Broadcast(DeliveryModels order);
+    }
+
+    public interface IAppConfirm
+    {
+        OfferModels Confirm(OfferModels order);
+    }
+
+    public interface IAutoConfirm
+    { 
+        DeliveryModels Confirm(DeliveryModels order);
+    }
+
+    public interface IInvoice
+    {
+    InvoiceModels Invoice(OfferModels order);
+    }
+
+    public interface IReceipt
+    {
+    ReceiptModels Receipt(DeliveryModels order);
+    }
+
+
+    //Offers = SignOff(Receipts)
+    public class AppModels // : IAppSignOff
+    {
+        public IBroadcastable Sign(ISignable offer)
+        {
+            offer.Sign();
+
+            throw new NotImplementedException();
+        }
+
+        public IConfirmable Broadcast(IBroadcastable offer) 
+        {
+            offer.Broadcast();
+
+            throw new NotImplementedException(); 
+        }
+
+        public void Confirm() 
+        { 
+            throw new NotImplementedException(); 
+        }
+
+        public AnInvoice Invoice(IInvoiceable offer) 
+        {
+            //var anInvoice = offer.Invoice();
+
+            throw new NotImplementedException(); 
+        }
+    }
+
+    //Offers = Broadcast(Offers)
+    public class OfferModels// : IOffer 
+    { //public IOfferConfirmable Broadcast();
+    }
+
+    //Offers = Confirm(Offers)
+    public class AppNetModels { }
+
+    //Invoices = Invoice(Offers)
+    public class InvoiceModels { }
+
+    //Deliveries SignOff(Invoices)
+    public class AutoModels { }
+
+    //Deliveries = Broadcast(Deliveries)
+    public class DeliveryModels { }
+
+    //Deliveries = Confirm(Deliveries)
+    public class AutoNetModels { }
+
+    //Receipts = Receipt(Deliveries)
+    public class ReceiptModels { }
+
+
+    //public interface IOfferConfirmater
+    public class Order2// : IUser2,
+    {
+        //IUsing2 User;
+
+        IOffer User(IOfferable order) { return null; }
+
+        IOfferConfirmable Offer(IOffer order) { return null; }
+
+        IInvoiceable Confirm(IOffer order) { return null; }
+
+        IInvoice Invoice(IInvoiceable order) { return null; }
+
+        IDelivery Deliver(IInvoice order) { return null; }
+
+        //IReceiptable Confirm();
+
+        IReceipt Receipt(IDelivery order) { return null; }
+    }
 }
     public class OrderModel
     {
